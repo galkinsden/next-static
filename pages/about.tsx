342d88037e8b2b782f99618c5f3ee92da03da87f
galkinsden <galkinsden@gmail.com>
2022-10-17 17:17:32 +0300

    init

---
+++ pages/about.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const About = () => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          DEMO NEXT
+        </h1>
+
+        <Link href="/">
+          <a>go back</a>
+        </Link>
+      </main>
+    </div>
+  )
+}
+
+export default About
